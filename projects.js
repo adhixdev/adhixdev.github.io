e035c58ace742c503c7f8be881026059d29130b4
Adithyan Santhakumar <adithyankmr@gmail.com>
2026-03-27 23:54:31 +0000

    Delete projects.js

--- projects.js
+++
@@ -1,7 +0,0 @@
-function openModal(id) {
-  document.getElementById(`modal-${id}`).classList.add('active');
-}
-
-function closeModal(id) {
-  document.getElementById(`modal-${id}`).classList.remove('active');
-}
